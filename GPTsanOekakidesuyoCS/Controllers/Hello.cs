bb2be384f2d0e0d1a8d9d3c3b381437cb3e5b4bb
yskym <yskym@YOSKY>
2023-04-29 13:58:06 +0900

    プロジェクト ファイルを追加します。

---
+++ GPTsanOekakidesuyoCS/Controllers/Hello.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace GPTsanOekakidesuyoCS.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        private readonly ILogger<HelloController> _logger;
+        public HelloController(ILogger<HelloController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetHelloController")]
+        public string Get() 
+        {
+            return "hello";
+        }
+    }
+}
